invertase/react-native-firebase

commit ffc2eca6da67edfb90f404d899fbbe51ed81d822

[js] add workaround for `module-resolver` issues - #1560

src/modules/firestore/DocumentSnapshot.js
@@ -8,7 +8,7 @@ import Path from './Path';
 import { isObject, deepGet } from '../../utils';
 import { parseNativeMap } from './utils/serialize';
 
-import type Firestore from '.';
+import type Firestore from './';
 import type {
   NativeDocumentSnapshot,
   SnapshotMetadata,
@@ -67,6 +67,6 @@ export default class DocumentSnapshot {
       return extractFieldPathData(this._data, fieldPath._segments);
     }
 
-    return deepGet(this._data, fieldPath, '.');
+    return deepGet(this._data, fieldPath, './');
   };
 }

.eslintrc
@@ -31,7 +31,8 @@
     "no-undef": 0,
     "no-underscore-dangle": "off",
     "no-use-before-define": 0,
-    "import/no-cycle": 0
+    "import/no-cycle": 0,
+    "import/no-useless-path-segments": 0
   },
   "globals": {
     "__DEV__": true,

src/modules/admob/Interstitial.js
@@ -1,10 +1,10 @@
 import { Platform } from 'react-native';
-import { statics } from '.';
+import { statics } from './';
 import AdRequest from './AdRequest';
 import { SharedEventEmitter } from '../../utils/events';
 import { getNativeModule } from '../../utils/native';
 import { nativeToJSError } from '../../utils';
-import type AdMob from '.';
+import type AdMob from './';
 
 let subscriptions = [];
 

src/modules/admob/RewardedVideo.js
@@ -1,9 +1,9 @@
-import { statics } from '.';
+import { statics } from './';
 import AdRequest from './AdRequest';
 import { SharedEventEmitter } from '../../utils/events';
 import { getNativeModule } from '../../utils/native';
 import { nativeToJSError } from '../../utils';
-import type AdMob from '.';
+import type AdMob from './';
 
 let subscriptions = [];
 

src/modules/auth/AuthSettings.js
@@ -1,7 +1,7 @@
 import { getNativeModule } from '../../utils/native';
 import { isAndroid, isIOS } from '../../utils';
 
-import type Auth from '.';
+import type Auth from './';
 
 /**
  * Interface representing an Auth instance's settings, currently used

src/modules/auth/User.js
@@ -5,7 +5,7 @@
 import INTERNALS from '../../utils/internals';
 import { getNativeModule } from '../../utils/native';
 
-import type Auth from '.';
+import type Auth from './';
 import type {
   ActionCodeSettings,
   AuthCredential,

src/modules/database/OnDisconnect.js
@@ -4,7 +4,7 @@
  */
 import { typeOf } from '../../utils';
 import { getNativeModule } from '../../utils/native';
-import type Database from '.';
+import type Database from './';
 import type Reference from './Reference';
 
 /**

src/modules/database/Reference.js
@@ -21,7 +21,7 @@ import {
 
 import SyncTree from '../../utils/SyncTree';
 
-import type Database from '.';
+import type Database from './';
 import type { DatabaseModifier, FirebaseError } from '../../types';
 
 // track all event registrations by path

NakamuraKuhiniko/react-native-firebase-app

commit 881105ccd72166fc17fecf49c33ce1679f63cdb1

fix regression caused by ffc2eca6da67edfb90f404d899fbbe51ed81d822

src/modules/firestore/DocumentSnapshot.js
@@ -67,6 +67,6 @@ export default class DocumentSnapshot {
       return extractFieldPathData(this._data, fieldPath._segments);
     }
 
-    return deepGet(this._data, fieldPath, './');
+    return deepGet(this._data, fieldPath, '.');
   };
 }